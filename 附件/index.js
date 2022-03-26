455bcc06e86ce788707cec57220c6df960dc3501
Genhao Li <ligenhao20010916@gmail.com>
2022-03-26 22:23:53 +0800

    task 1~5

--- 附件/index.js
+++ 附件/index.js
@@ -8,13 +8,52 @@ window.onload = function() {
         search();
     });
 
-    // TODO: 在此为 top-right 元素设置监听器
-    // document.getElementById('top-right') ...
+    document.getElementById('top-right').addEventListener('click', function () {
+        clickLogin();
+    })
+
+
+
+    let hotList = document.getElementById("hot");
+    let modelHTML = hotList.innerHTML;
+    let str = '';
+    let games = ['原神', '香肠派对', '王者荣耀','我的世界','和平精英','植物大战僵尸','迷你世界','光遇','泰拉瑞亚','绝地求生'];
+    let hotValues = [324853, 168931, 161167, 127320, 123379, 88496, 79547, 76194, 54000, 49095];
+    let hengIndices = [2, 4, 7];
+    for(let i = 0; i < 10; i++) {
+        let model = createElementFromHTML(modelHTML);
+        let orderElem = model.getElementsByClassName('hot-order')[0];
+        orderElem.innerHTML = (i+1).toString();
+        orderElem.classList.remove('hot-order-1');
+        if (i < 3) {
+            orderElem.classList.add(`hot-order-${i+1}`);
+        }
+
+        model.getElementsByClassName('hot-content')[0].innerHTML = games[i];
+        model.getElementsByClassName('hot-value')[0].innerHTML = hotValues[i].toString();
+
+        let imgElem = model.getElementsByClassName('status-icon')[0];
+        if (hengIndices.indexOf(i) >= 0){
+            imgElem.setAttribute('src', 'img/heng.png');
+        }
+
+        str += model.outerHTML;
+    }
+    hotList.innerHTML = str;
+    console.log(hotList);
+}
+
+function createElementFromHTML(htmlString) {
+    let div = document.createElement('div');
+    div.innerHTML = htmlString.trim();
+
+    // Change this to div.childNodes to support multiple top-level nodes.
+    return div.firstElementChild;
 }
 
 function search() {
-    // TODO: 搜索触发后的行为
-    console.log('');
+    let keyword = document.getElementById("keyword").value;
+    alert(keyword.length === 0 ? "请输入搜索内容" : keyword);
 }
 
 function clickLogin() {
@@ -28,14 +67,15 @@ function clickLogin() {
 
 function initUserInfo() {
     // TODO: 修改页面显示错误的 bug，另外注意图片路径是否正确
-    var username = Kernal.getUserName();
-    var content = '<div id="user">\
+    let username = Kernal.getUserName();
+    let content = '<div id="user">\
                         <span id="user-img">\
                             <img src="img/user.jpg" />\
                         </span>\
-                        <span id="name">' + username + '</span>\
+                        <span id="name">' + '</span>\
                     </div>';
     document.getElementById('top-right').innerHTML = content;
+    document.getElementById('name').textContent = username;
 }
 
 // ============================================================ 你不需要去关注的代码
